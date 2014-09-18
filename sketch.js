4a675e76398fb0a0b9c5c2c0a8be3abf2760fcf3
kangdongkil <kbk3418@naver.com>
2014-09-18 16:37:43 +0900

    a

--- sketch.js
+++ sketch.js
@@ -1,31 +1,28 @@
 function setup() {
-  createCanvas(640, 480);
-  background(128, 0, 64);
-  noFill();
-
-  stroke(255, 0, 0);
-
-  var x;
-/*
-  x = 1;
-  ellipse(x*150, 100, 50, 50);
-
-  x = 2;
-  ellipse(x*150, 100, 50, 50);
-
-  x = 3;
-  ellipse(x*150, 100, 50, 50);
-}
-*/
-//for(시작상태;반복하는 조건; 증가값)
-for(var x = 1; x<=7; x++)
-	{
-	ellipse(x*70,x*20 + 100, x * 30, x * 90);
-	}
+   createCanvas(640, 400);
+   
+   backgroundColor = color(0, 0, 0, 255);
+   fillColor = color(255,0, 0, 128);
 }
 
-function draw(){
-/*var size-Math.abs(mouseX -320);*/
-background(128,0,64);
-ellipse(320, 240, mouseX, mouseY);
+function draw() {
+   background(backgroundColor);
+   
+   fillColor = color(
+      Math. random() *255,
+      Math. random() *255, 
+      Math. random() *255, 
+      Math. random() *255);
+
+   mouseColor = color(
+      mouseX / 640 * 128 + Math. random() * 128,
+      mouseY / 480 * 255, 0);
+
+// fill(fillColor);
+// rect(100, 100, 100, 100);
+
+   fill(mouseColor);
+   rect(100, 100, 100, 100);
+   ellipse(300,200,100,100);
 }
+
